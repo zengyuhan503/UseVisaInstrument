4f60023d9feaca15e0d7eaab4579b832221da51a
ZENGYUHAN <zengyuhan@skyworth.com>
2024-07-31 11:33:05 +0800

    关联：https://juejin.cn/post/7397326977424572427

--- src-tauri/src/use_visa_instrument.rs
+++ src-tauri/src/use_visa_instrument.rs
@@ -85,54 +85,100 @@ pub struct VisaInstrument {
  * @param instr_ip: 设备ip
  */
 impl VisaInstrument {
+    /// 创建一个新的Visa 仪器实例。
+    ///
+    /// 这个函数尝试根据提供的仪器IP地址初始化一个DEFAULTR资源管理器的实例。
+    /// 它首先尝试获取资源管理器的锁，然后根据IP地址构造一个资源表达式，
+    /// 接着查找和打开相应的资源，最后返回一个新的DEFAULTR实例。
+    ///
+    /// # 参数
+    /// `instr_ip` - 仪器的IP地址，用于构造资源表达式。
+    ///
+    /// # 返回值
+    /// 返回一个`Result`类型，其中包含初始化成功的DEFAULTR实例或错误信息。
     pub fn new(instr_ip: String) -> Result<Self, String> {
+        // 日志记录初始化开始
         log::info!("初始化DEFAULTR 实例");
-        let rm = RESOURCE_DEFAULTRM.lock().map_err(|e| format!("{:?}", e))?;
+        // 尝试获取资源管理器的锁，如果失败，则格式化错误并返回
+        let rm = RESOURCE_DEFAULTRM.lock().map_err(|e| format!("{:?}", e))?; 
+        // 日志记录创建CString实例开始
         log::info!("创建CString 实例");
+        // 构造资源表达式
         let ip_s = format!("TCPIP0::{}::inst0::INSTR", instr_ip);
+        // 尝试将构造的字符串转换为CString，如果失败，则格式化错误并返回
         let expr = CString::new(ip_s)
             .map_err(|err| format!("没有找到相关资源：{:?}", err))?
             .into();
 
+        // 在资源管理器中查找资源，如果失败，则格式化错误并返回
         let rsc = rm
             .find_res(&expr)
             .map_err(|op| format!("未找到相关的资源{:?}", op))?;
 
+        // 使用找到的资源打开仪器，如果失败，则格式化错误并返回
         let instr = rm
             .open(&rsc, AccessMode::NO_LOCK, TIMEOUT_IMMEDIATE)
             .map_err(|op| format!("{:?}", op))?;
+
+        // 返回初始化成功的DEFAULTR实例
         Ok(Self { instr })
     }
+    /// 查找所有资源的函数
+    /// 
+    /// 返回结果为包含VisaString类型资源列表的Result对象，如果发生错误，则错误信息为String类型。
     pub fn find_all_resources() -> Result<Vec<VisaString>, String> {
-        let rm = RESOURCE_DEFAULTRM.lock().map_err(|e| format!("{:?}", e))?;
-        let expr = CString::new("?*INSTR").map_err(|err| format!("没有找到相关资源：{:?}", err))?;
+        // 尝试获取RESOURCE_DEFAULTRM的锁，如果失败，则转换错误信息并返回
+        let rm = RESOURCE_DEFAULTRM.lock().map_err(|e| format!("{:?}", e))?; 
+        // 创建CString对象，用于后续的资源查找，如果创建失败，则转换错误信息并返回
+        let expr = CString::new("?*INSTR").map_err(|err| format!("没有找到相关资源：{:?}", err))?; 
+        // 初始化一个空的VisaString向量，用于存放查找结果
         let mut visa_list: Vec<VisaString> = Vec::new();
+        // 使用rm对象和表达式查找资源列表
         let list = rm.find_res_list(&expr.into());
+        // 根据查找结果进行处理
         match list {
+            // 如果查找成功，遍历结果列表
             Ok(list) => {
                 for item in list {
+                    // 对于每个查找结果项，根据结果进行处理
                     match item {
+                        // 如果结果项成功，将其添加到visa_list中
                         Ok(vs) => {
                             visa_list.push(vs);
                         }
+                        // 如果结果项失败，打印错误信息
                         Err(err) => {
                             println!("error:{:?}", err)
                         }
                     }
                 }
             }
+            // 如果查找失败，返回错误信息
             Err(err) => {
                 return Err(format!("未找到相关的资源{:?}", err));
             }
         }
+        // 打印查找结果
+        println!("visa_list:{:?}",visa_list);
+        // 返回包含查找结果的Ok对象
         Ok(visa_list)
     }
+    /// 异步关闭所有设备。
+    ///
+    /// 此方法通过发送一条指令来关闭设备输出，然后清除指令缓冲区。
+    /// 它返回一个结果，指示关闭操作是否成功。
     pub async fn close_all(&mut self) -> Result<(), String> {
+        // 构造关闭输出的指令字符串
         let cmds = format!("OUTP OFF,(@1:3)\n");
+        // 将指令字符串转换为字节序列，以备写入
         let cmd = cmds.as_bytes();
+        // 获取指令写入器的引用
         let mut write_instr = &self.instr;
+        // 尝试写入指令，并处理可能的错误
         let res = write_instr.write_all(cmd).map_err(|err| format!("{}", err));
+        // 清除指令缓冲区，确保后续操作不会受到本次操作的影响
         let _ = self.instr.clear();
+        // 返回操作结果
         res
     }
     pub fn set_config(&mut self, item: String) -> Result<(), String> {
--- src/utlis/actionCallback.ts
+++ src/utlis/actionCallback.ts
@@ -6,6 +6,20 @@ interface ActionParams {
     item1: any,
     item2: string
 }
+
+// 这个TypeScript代码片段定义了一些函数，用于与Tauri应用程序中的事件和API调用进行交互。
+
+// ActionDataAsyncCallback函数是一个异步回调函数，用于执行某个操作并返回结果。它接受三个参数：name、item和item2。函数返回一个Promise对象，用于处理操作的成功或失败。内部逻辑是：
+
+// 监听名为action_res的事件，当事件触发时，检查返回的数据中是否包含error，如果有则调用reject并返回错误信息；
+// 如果没有错误，调用resolve并返回数据；
+// 在监听之前，通过emit函数发送一个名为action_req的事件，并附带操作的参数。
+// ACtionDataCallback函数用于执行某个操作并在操作完成后调用回调函数。它接受四个参数：name、item、item2和callback。函数内部逻辑是：
+
+// 如果已经存在一个监听器unlisten，则先停止之前的监听；
+// 监听名为action_data的事件，当事件触发时，调用传入的回调函数并传入事件的负载数据；
+// 通过emit函数发送一个名为action_req的事件，并附带操作的参数。
+
 export const ActionDataAsyncCallback = (name: string, item: any, item2: string) => {
     return new Promise(async (resolve, reject) => {
         try {
